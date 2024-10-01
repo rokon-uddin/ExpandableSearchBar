02cec5986c410649f7affceace484316121999dd
Mohammed Rokon Uddin <shoaib.rokon@gmail.com>
2024-10-01 17:00:30 +0600

    Initial Commit

---
+++ ExpandableSearchBar/ExpandableSearchBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpandableSearchBarApp.swift
+//  ExpandableSearchBar
+//
+//  Created by Mohammed Rokon Uddin on 10/1/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpandableSearchBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
